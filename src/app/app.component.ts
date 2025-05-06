cd9f183d5cb4c7c5575ce68b557cf4c5c9f8ac4c
Bobylev E. <102691182+NonEstSpes@users.noreply.github.com>
2025-05-06 08:55:37 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FanClubTutorialComponent } from './fan-club-tutorial/fan-club-tutorial.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FanClubTutorialComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'test-music';
---
+++ src/app/fan-club-tutorial/fan-club-tutorial.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fan-club-tutorial',
+  imports: [],
+  templateUrl: './fan-club-tutorial.component.html',
+  styleUrl: './fan-club-tutorial.component.scss',
+})
+export class FanClubTutorialComponent {}
